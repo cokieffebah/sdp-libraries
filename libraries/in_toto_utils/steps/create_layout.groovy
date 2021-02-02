c321d62544333d922ca3b06c005237bf6adc6090
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-02 16:40:41 -0500

    Initial add of collection code

--- libraries/in_toto_utils/steps/create_layout.groovy
+++ libraries/in_toto_utils/steps/create_layout.groovy
@@ -2,7 +2,11 @@ package libraries.in_toto_utils
 
 @CleanUp
 void call(){
-
+    println "pipelineConfig.intotoCollector: ${pipelineConfig.intotoCollector}"
+    
+    pipelineConfig.intotoCollector.each{ c ->
+        println "for ${c}: pipelineConfig.libraries[${c.library}].in_toto[${c.step}]: ${pipelineConfig.libraries[c.library].in_toto[c.step]}"
+    }
 }
 
 List get_steps(){
