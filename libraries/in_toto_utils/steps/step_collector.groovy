3309e6c706c6d354a69301b49cb1ad994d20306e
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-02 15:00:54 -0500

    fixed pipelineConfig var

--- libraries/in_toto_utils/steps/step_collector.groovy
+++ libraries/in_toto_utils/steps/step_collector.groovy
@@ -3,5 +3,5 @@ package libraries.in_toto_utils
 @BeforeStep()
 void call(){
     String queue_name = config.step_queue_name ?: "intotoCollector"
-    pipeline_config[queue_name] << [step: hookContext.step, library: hookContext.library]
+    pipelineConfig[queue_name] << [step: hookContext.step, library: hookContext.library]
 }
