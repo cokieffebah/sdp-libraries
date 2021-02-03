72b271e76902c6f95b9ee10ae443d8e5325685bd
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-03 16:39:36 -0500

    re-enable docker.image.inside

--- libraries/in_toto_utils/steps/wrap_around.groovy
+++ libraries/in_toto_utils/steps/wrap_around.groovy
@@ -1,11 +1,11 @@
 package libraries.in_toto_utils
 
 void call(body){
-   //docker.image(config.inside_image).inside {
+   docker.image(config.inside_image).inside {
        //record_start( [:], config)
        body()
        //record_stop( [:], config)
-   //}
+   }
 }
 
 
