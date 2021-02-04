1200d6e36181c6e00b7f35febab576b6892b9abc
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-04 13:29:03 -0500

    reverting to original design

--- libraries/in_toto_ex/steps/scan.groovy
+++ libraries/in_toto_ex/steps/scan.groovy
@@ -1,9 +1,6 @@
 package libraries.in_toto_ex
 
 void call(){
-   docker.image("in-toto-python:demo").inside{
-      echo ""
-   }
    println "printing layout for scan:"
    println get_intoto_layout('scan')
 }
