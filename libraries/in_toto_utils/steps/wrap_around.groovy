e879e952ff32ab0716bd3321c2be54b51297c98a
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-04 11:24:44 -0500

    debug

--- libraries/in_toto_utils/steps/wrap_around.groovy
+++ libraries/in_toto_utils/steps/wrap_around.groovy
@@ -1,12 +1,13 @@
 package libraries.in_toto_utils
 
 void call(body){
-   docker.image(config.inside_image).inside {
-       record_start( [:], config)
-       body()
-       record_stop( [:], config)
-   }
-   return
+//    docker.image(config.inside_image).inside {
+//        record_start( [:], config)
+//        body()
+//        record_stop( [:], config)
+//    }
+   x = 5
+   body()
 }
 
 
