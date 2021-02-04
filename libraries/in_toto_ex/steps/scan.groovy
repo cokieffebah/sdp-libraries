febb8ab7e3da1f7f42a0ba238b758aee6b419e6f
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-04 11:28:01 -0500

    debug

--- libraries/in_toto_ex/steps/scan.groovy
+++ libraries/in_toto_ex/steps/scan.groovy
@@ -1,6 +1,9 @@
 package libraries.in_toto_ex
 
 void call(){
+   docker.image(config.inside_image).inside{
+      echo ""
+   }
    println "printing layout for scan:"
    println get_intoto_layout('scan')
 }
