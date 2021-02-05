909a89261c24b60dd3c789be87c09a8d1540726d
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-05 16:50:48 -0500

    changed is_collectable to can_collect

--- libraries/in_toto_utils/steps/step_collector.groovy
+++ libraries/in_toto_utils/steps/step_collector.groovy
@@ -2,6 +2,6 @@ package libraries.in_toto_utils
 
 @BeforeStep( { hookContext.library != "in_toto_utils" })
 void call(){
-    if( get_collector.is_collectable( hookContext.library, hookContext.step) )
+    if( get_collector.can_collect( hookContext.library, hookContext.step) )
         get_collector() << [step: hookContext.step, library: hookContext.library]
 }
