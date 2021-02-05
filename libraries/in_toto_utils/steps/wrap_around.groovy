cd02006c393623e22e9d85d12c9151adae9e3600
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-05 17:12:23 -0500

    update

--- libraries/in_toto_utils/steps/wrap_around.groovy
+++ libraries/in_toto_utils/steps/wrap_around.groovy
@@ -4,7 +4,7 @@ void call(String step, body = {}){
 
     docker.image(config.inside_image).inside {
         unstash 'workspace'
-        record_start( [step: step, key: 'bob'], config)
+        record_start( step )
         body()
         record_stop( [step: step, key: 'bob'], config)
         stash 'workspace'
@@ -12,14 +12,16 @@ void call(String step, body = {}){
 }
 
 
-void record_start(Map args, Map config){
-    // List cmd = ["in-toto-record start --verbose"]
-    // cmd << " --step-name ${args.step}"
-    // if( args.materials ){
-    //   cmd << " --materials ${args.materials}"
-    // }
-    // cmd << " --key ${args.key}"
-    // sh( script: cmd.join("") )
+void record_start(step){
+    def stepWrapper = getBinding().getStep(step)
+    Map args = get_collector.step_config( stepWrapper.library, step)
+    List cmd = ["in-toto-record start --verbose"]
+    cmd << " --step-name ${step}"
+    if( args.materials ){
+      cmd << " --materials ${args.materials}"
+    }
+    cmd << " --key ${args.key}"
+    sh( script: cmd.join("") )
 }
 
 void record_stop(Map args, Map config){
