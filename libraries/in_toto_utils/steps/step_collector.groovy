c96b3f00e990dc9d441a8fedaaed7d1bd31096f6
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-05 14:10:40 -0500

    ignoring its own library

--- libraries/in_toto_utils/steps/step_collector.groovy
+++ libraries/in_toto_utils/steps/step_collector.groovy
@@ -1,8 +1,7 @@
 package libraries.in_toto_utils
 
-@BeforeStep()
+@BeforeStep( { hookContext.library != "in_toto_utils" })
 void call(){
-    
     if( is_collectable( hookContext.library, hookContext.step) )
         get_collector() << [step: hookContext.step, library: hookContext.library]
 }
