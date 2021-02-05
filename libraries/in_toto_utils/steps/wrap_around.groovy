18c3db622f7b0ac16ddb51e0561e2819cbdb40a7
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-05 14:34:11 -0500

    adding args to wrap_around

--- libraries/in_toto_utils/steps/wrap_around.groovy
+++ libraries/in_toto_utils/steps/wrap_around.groovy
@@ -1,17 +1,21 @@
 package libraries.in_toto_utils
 
-void call(body){
-   docker.image(config.inside_image).inside {
-       unstash 'workspace'
-       record_start( [:], config)
-       body()
-       record_stop( [:], config)
-   }
+void call(Map args = [:], body = {}){
+    Map start_args = args.start ?: [:]
+    Map stop_args = args.stop ?: [:]
+
+    docker.image(config.inside_image).inside {
+        unstash 'workspace'
+        record_start( start_args, config)
+        body()
+        record_stop( stop_args, config)
+        stash 'workspace'
+    }
 }
 
 
 void record_start(Map args, Map config){
-
+    
 }
 
 void record_stop(Map args, Map config){
