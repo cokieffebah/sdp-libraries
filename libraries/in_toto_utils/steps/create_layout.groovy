2324b3e7e3a5bb42e3bdbe2adec405d1de16bcc1
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-05 09:59:03 -0500

    adding skip libs

--- libraries/in_toto_utils/steps/create_layout.groovy
+++ libraries/in_toto_utils/steps/create_layout.groovy
@@ -1,15 +1,29 @@
 package libraries.in_toto_utils
 
+@CleanUp
 void call(){
     println "pipelineConfig.intotoCollector: ${pipelineConfig.intotoCollector}"
+    List skip_libs = get_skip_libs()
     
     pipelineConfig.intotoCollector.each{ c ->
+        if( c.library in skip_libs){
+          println "skipping: ${c}: pipelineConfig.libraries[${c.library}"
+          break
+        }
+
         if( pipelineConfig.libraries[c.library].in_toto ){
           println "for ${c}: pipelineConfig.libraries[${c.library}].in_toto[${c.step}]: ${pipelineConfig.libraries[c.library].in_toto[c.step]}"
         } else {
           println "for ${c}: null == pipelineConfig.libraries[${c.library}].in_toto"
         }
     }
+}
+
+List get_skip_libs(){
+  List libs = config?.create_layout?.extra_skip_libs ?: []
+  libs << "in_toto_utils"
+
+  return libs
 }
 
 List get_steps(){
