0646b3a365e47705e07548f840c0daff7e233afa
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-05 16:34:21 -0500

    testing step call

--- libraries/in_toto_utils/steps/wrap_around.groovy
+++ libraries/in_toto_utils/steps/wrap_around.groovy
@@ -1,12 +1,10 @@
 package libraries.in_toto_utils
 
-void call(Map args = [:], body = {}){
-    Map start_args = args.start ?: [:]
-    Map stop_args = args.stop ?: [:]
+void call(String step, body = {}){
 
     docker.image(config.inside_image).inside {
         unstash 'workspace'
-        record_start( start_args, config)
+        record_start( [step: step, key: 'bob'], config)
         body()
         record_stop( stop_args, config)
         stash 'workspace'
@@ -15,7 +13,13 @@ void call(Map args = [:], body = {}){
 
 
 void record_start(Map args, Map config){
-    
+    List cmd = ["in-toto-record start --verbose"]
+    cmd << " --step-name ${args.step}"
+    if( args.materials ){
+      cmd << " --materials ${args.materials}"
+    }
+    cmd << " --key ${args.key}"
+    sh( script: cmd.join("") )
 }
 
 void record_stop(Map args, Map config){
