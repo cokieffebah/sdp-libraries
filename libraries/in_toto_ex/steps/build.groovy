190648aca0892d98da59f39939ddbfef2b4aac29
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-08 11:24:13 -0500

    adding fake content since git commands wont work

--- libraries/in_toto_ex/steps/build.groovy
+++ libraries/in_toto_ex/steps/build.groovy
@@ -1,10 +1,9 @@
 package libraries.in_toto_ex
 
 void call(){
-   println "printing layout for build:"
-   println get_intoto_layout('build')
-   //println "\nprinting layout with no argument:"
-   //println get_intoto_layout()
-   sh( script: "git clone https://github.com/in-toto/demo-project.git")
-   sh( script: "git log --pretty=oneline > demo-project/vcs.log")
+   sh( "mkdir demo-project")
+   sh( "echo 'fake vcs-log' > demo-project/vsc.log")
+   // the wrapper would need 'git'
+   // sh( script: "git clone https://github.com/in-toto/demo-project.git")
+   // sh( script: "git log --pretty=oneline > demo-project/vcs.log")
 }
