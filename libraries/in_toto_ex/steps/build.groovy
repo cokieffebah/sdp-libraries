e77eba999c5791e5e69093c4b11a415427aa4c1b
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-08 09:27:22 -0500

    updating build to do something

--- libraries/in_toto_ex/steps/build.groovy
+++ libraries/in_toto_ex/steps/build.groovy
@@ -3,6 +3,8 @@ package libraries.in_toto_ex
 void call(){
    println "printing layout for build:"
    println get_intoto_layout('build')
-   println "\nprinting layout with no argument:"
-   println get_intoto_layout()
+   //println "\nprinting layout with no argument:"
+   //println get_intoto_layout()
+   sh( script: "git clone https://github.com/in-toto/demo-project.git")
+   sh( script: "git log --pretty=oneline > demo-project/vcs.log")
 }
