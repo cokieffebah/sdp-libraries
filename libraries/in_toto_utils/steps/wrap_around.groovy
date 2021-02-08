596259400882282ef25f6f1b2724f57609c4e2ad
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-08 09:21:42 -0500

    adding record stop

--- libraries/in_toto_utils/steps/wrap_around.groovy
+++ libraries/in_toto_utils/steps/wrap_around.groovy
@@ -6,24 +6,32 @@ void call(String step, body = {}){
         unstash 'workspace'
         record_start( step )
         body()
-        record_stop( [step: step, key: 'bob'], config)
+        record_stop( step )
         stash 'workspace'
     }
 }
 
 
-void record_start(step){
+void record_start(String step){
     def stepWrapper = getBinding().getStep(step)
     Map args = get_collector.step_config( stepWrapper.library, step)
     List cmd = ["in-toto-record start --verbose"]
+    cmd << "--step-name ${step}"
+    if( args.materials ){
+      cmd << "--materials ${args.materials}"
+    }
+    cmd << "--key ${args.key}"
+    sh( script: cmd.join(" ") )
+}
+
+void record_stop(String step){
+    def stepWrapper = getBinding().getStep(step)
+    Map args = get_collector.step_config( stepWrapper.library, step)
+    List cmd = ["in-toto-record stop --verbose"]
     cmd << " --step-name ${step}"
     if( args.materials ){
-      cmd << " --materials ${args.materials}"
+      cmd << " --products ${args.products}"
     }
     cmd << " --key ${args.key}"
     sh( script: cmd.join("") )
-}
-
-void record_stop(Map args, Map config){
-
 }
