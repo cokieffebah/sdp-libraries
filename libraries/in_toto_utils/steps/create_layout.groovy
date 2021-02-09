7b7e9b8969b72405a1ff10158e3ba6e595d60969
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-09 13:24:34 -0500

    writing layout_json

--- libraries/in_toto_utils/steps/create_layout.groovy
+++ libraries/in_toto_utils/steps/create_layout.groovy
@@ -5,9 +5,14 @@ void call(){
   List collector = get_collector()
   println "pipelineConfig.intotoCollector: ${collector}"
 
+  Map layout_json = [_type:"layout"]
+  List steps = layout_json.steps = []
+
   // using for because I wanted 'continue'
   for( c in collector ){
       if( get_collector.can_collect(c.library, c.step) ){
+        steps << get_collector.layout_config(c.library, c.step)
+        steps[steps.size() - 1].name = c.step
         println "for ${c}: ${get_collector.layout_config(c.library, c.step)}"
       } else {
         println "for ${c}: null"
@@ -16,6 +21,7 @@ void call(){
 
   node{
     unstash 'workspace'
+    writeJSON( json: layout_json, file: "layout.json", pretty:4)
     writeFile( file:"create_layout.py", text: resource("create_layout.py"))
     stash 'workspace'
   }
