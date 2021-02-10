12d542cdd055062546b0c645fd289353f316a538
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-10 09:48:19 -0500

    using intoto_wrap in intoto_record

--- libraries/in_toto_utils/steps/intoto_record.groovy
+++ libraries/in_toto_utils/steps/intoto_record.groovy
@@ -1,18 +1,13 @@
 package libraries.in_toto_utils
 
 void call(String step, body = {}){
-    String workspace = config.workspace ?: 'workspace'
-    docker.image(config.inside_image).inside {
-        unstash workspace
+    get_collector.intoto_wrap {
         get_collector.generate_functionary_keys()
         record_start( step )
         body()
         record_stop( step )
-        stash workspace
     }
 }
-
-
 
 void record_start(String step){
     def stepWrapper = getBinding().getStep(step)
@@ -46,22 +41,4 @@ void record_stop(String step){
     
     cmd << "--key ${config.functionary.path}"
     sh( script: cmd.join(" ") )
-}
-
-String expectedListToString(String cmdArg, List expects){
-    List output = []
-    expects.each{ ex ->
-        def current = ex
-        if( ex instanceof List ){
-            current = []
-            ex.each{ ex1 ->
-                current << ex1
-            }
-
-            current = current.join(" ")
-        }
-        output << "${current}".toString()
-    }
-
-    return cmdArg + " " + output.join(" ")
 }
