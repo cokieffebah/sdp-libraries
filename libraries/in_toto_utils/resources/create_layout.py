41765a31713bb045cd73ab0cf8c5c83f6928d511
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-11 14:35:29 -0500

    adding arguments for input json and output layout

--- libraries/in_toto_utils/resources/create_layout.py
+++ libraries/in_toto_utils/resources/create_layout.py
@@ -7,7 +7,13 @@ import argparse
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-f", "--funcpath", type=str, default=None, 
-      help="the path to functionary key")
+    help="the path to functionary key")
+
+  parser.add_argument("-o", "--output", type=str, default="the.layout", 
+    help="the name for the output file") 
+
+  parser.add_argument("-j", "--inputjson", type=str, default="layout.json", 
+    help="the name for the input json file")      
 
   parser.add_argument("signerpath", type=str,
    help="the path to the signer key")    
@@ -19,15 +25,15 @@ def main():
   if args.funcpath:
     func_path = args.funcpath
 
-  with open('layout.json') as f:
+  with open(args.inputjson) as f:
     read_data = f.read()
     print('in-toto.json: ' + read_data)
     config_json = json.loads(read_data)
-    create_layout(config_json, signer_path, func_path)
+    create_layout(config_json, signer_path, func_path, args.output)
     print('create_layout.main')
 
 
-def create_layout(config_json, signer_path, func_path): 
+def create_layout(config_json, signer_path, func_path, layout_name): 
   config_json["keys"] = {} 
 
   func_key_data = interface.import_rsa_publickey_from_file(func_path)
@@ -42,7 +48,7 @@ def create_layout(config_json, signer_path, func_path):
 
   signer_key = interface.import_rsa_privatekey_from_file(signer_path)
   metadata.sign(signer_key)
-  metadata.dump("the.layout")
+  metadata.dump(layout_name)
   print('created the.layout')
 
 
