e46febce207ff3ed0134de9aa949d42c460b04dc
Carlos OKieffe <okieffe_carlos@bah.com>
2021-02-26 12:10:16 -0500

    oops

--- libraries/in_toto_ex/steps/package_image.groovy
+++ libraries/in_toto_ex/steps/package_image.groovy
@@ -1,9 +1,5 @@
 package libraries.in_toto_ex
 
 void call(){
-    String registry_url = config.registry_url ?: localhost:5000
-    String build_tag = config.build_image_tag ?: "intoto-demo:latest"
-    String build_dockerfile = config.build_image_dockerfile ?: "porter/porter.Dockerfile"
-  sh(script:"docker build -t ${build_tag} -f ${build_dockerfile} .")
-  sh(script:"docker image tag ${build_tag} ${registry_url}/${build_tag}")
+    
 }
