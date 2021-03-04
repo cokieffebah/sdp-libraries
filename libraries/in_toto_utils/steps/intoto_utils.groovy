9f5746b3df080e384006bf6dbf13984ef1b87168
Carlos OKieffe <okieffe_carlos@bah.com>
2021-03-04 09:36:16 -0500

    debug

--- libraries/in_toto_utils/steps/intoto_utils.groovy
+++ libraries/in_toto_utils/steps/intoto_utils.groovy
@@ -75,9 +75,8 @@ void write_functionary_keys(String functionary_path = null){
     }
 
     if( config.functionary.public_cred ){
-        withCredentials([file(credentialsId: config.functionary.public_cred, variable: 'publicKey')]) {
-            
-            writeFile( file:"${functionary_path}.pub" , text:publicKey )
+        withCredentials([file(credentialsId: config.functionary.public_cred, variable: 'publicKeyFile')]) {     
+            sh( "cp -p ${publicKeyFile} ${functionary_path}.pub")
             archiveArtifacts( artifacts: "${functionary_path}.pub" )
         }
     }
