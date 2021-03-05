1b7b2cd23ea35cfda009f574273da7c8e8a29927
Carlos OKieffe <okieffe_carlos@bah.com>
2021-03-05 16:40:00 -0500

    removing in-toto wrap from default intoto_record

--- libraries/in_toto_utils/steps/intoto_record.groovy
+++ libraries/in_toto_utils/steps/intoto_record.groovy
@@ -1,12 +1,20 @@
 package libraries.in_toto_utils
 
 void call(String step, body = {}){
+    std(step, body)
+}
+
+void wrapped(String step, body = {}){
     intoto_utils.intoto_wrap {
         intoto_utils.generate_functionary_keys()
-        record_start( step )
-        body()
-        record_stop( step )
+        std(step, body)
     }
+}
+
+void std(String step, body = {}){
+    record_start( step )
+    body()
+    record_stop( step )
 }
 
 void record_start(String step){
