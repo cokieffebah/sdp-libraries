2d943b85b91807a8bf1f252e102e60929a579ef5
Carlos OKieffe <okieffe_carlos@bah.com>
2021-03-15 11:17:22 -0400

    adding block to if

--- libraries/in_toto_utils/steps/step_collector.groovy
+++ libraries/in_toto_utils/steps/step_collector.groovy
@@ -2,6 +2,7 @@ package libraries.in_toto_utils
 
 @BeforeStep( { hookContext.library != "in_toto_utils" })
 void call(){
-    if( intoto_utils.can_collect( hookContext.library, hookContext.step) )
+    if( intoto_utils.can_collect( hookContext.library, hookContext.step) ){
         intoto_utils.get_collector() << [step: hookContext.step, library: hookContext.library]
+    }
 }
