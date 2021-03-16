b5d12db36417ae7df5c1db425103f54810785673
Carlos OKieffe <okieffe_carlos@bah.com>
2021-03-16 14:37:46 -0400

    fixed to vcs.log from vsc.log

--- libraries/in_toto_ex/steps/build_app.groovy
+++ libraries/in_toto_ex/steps/build_app.groovy
@@ -2,7 +2,7 @@ package libraries.in_toto_ex.steps
 
 void call(){
    sh( "mkdir demo-project")
-   sh( "echo 'fake vcs-log' > demo-project/vsc.log")
+   sh( "echo 'fake vcs-log' > demo-project/vcs.log")
    // the wrapper would need 'git'
    // sh( script: "git clone https://github.com/in-toto/demo-project.git")
    // sh( script: "git log --pretty=oneline > demo-project/vcs.log")
