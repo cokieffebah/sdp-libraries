6b1ea38b6f72446d69895754e20449ce0ed71fa4
Carlos OKieffe <okieffe_carlos@bah.com>
2021-03-16 14:05:19 -0400

    removing link print

--- libraries/in_toto_ex/steps/scan.groovy
+++ libraries/in_toto_ex/steps/scan.groovy
@@ -1,7 +1,7 @@
 package libraries.in_toto_ex.steps
 
 void call(){
-   println "printing layout for scan:"
-   println get_intoto_layout('scan')
+   //println "printing layout for scan:"
+   //println get_intoto_layout('scan')
    sh("ls -ltra . > scan.log")
 }
