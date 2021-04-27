ca2acc4d3aceb0ea5487b374fa40695c5f8d4bda
Carlos OKieffe <okieffe_carlos@bah.com>
2021-04-27 12:27:41 -0400

    adding verify_image to test its existence

--- libraries/in_toto_utils/steps/create_layout.groovy
+++ libraries/in_toto_utils/steps/create_layout.groovy
@@ -113,7 +113,7 @@ void verify_layout(String layout_key_path = null, String layout_file = null, Str
 
   dir( final_dir ){// finalDir 
     body()
-    sh("/workspace/bin/verify_image.sh ${relative_repo_path} ${app_image_digest}")
+    sh("/workspace/bin/verify_image.sh")
     sh("ls -l")
     def status = sh(returnStatus: true, script: "in-toto-verify ${ verbose ? '--verbose' : ''} --layout ${layout_file} --layout-key ${layout_key_path}")
     println ""
