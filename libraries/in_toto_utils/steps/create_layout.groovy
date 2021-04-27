4d464271f89b15e52a5861ae21ca894b7a776bb5
Carlos OKieffe <okieffe_carlos@bah.com>
2021-04-27 12:23:55 -0400

    debugging verify_image

--- libraries/in_toto_utils/steps/create_layout.groovy
+++ libraries/in_toto_utils/steps/create_layout.groovy
@@ -113,6 +113,8 @@ void verify_layout(String layout_key_path = null, String layout_file = null, Str
 
   dir( final_dir ){// finalDir 
     body()
+    sh("/workspace/bin/verify_image.sh ${relative_repo_path} ${app_image_digest}")
+    sh("ls -l")
     def status = sh(returnStatus: true, script: "in-toto-verify ${ verbose ? '--verbose' : ''} --layout ${layout_file} --layout-key ${layout_key_path}")
     println ""
     println "completed in-toto-verify.status: ${status}"
